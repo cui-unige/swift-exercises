e697189e1b1847ac416a3a5eb7ea3c36fba38fd2
kaikoveritch <kaiko.goren@gmail.com>
2017-02-23 22:32:53 +0100

    Basically a whole lot of debugging... Also, a table of types's resistances/weaknesses and an associated function have been added.

--- Sources/swift_exercises.swift
+++ Sources/swift_exercises.swift
@@ -1,3 +1,6 @@
+// For the 'floor' function
+import Foundation
+
 // http://bulbapedia.bulbagarden.net/wiki/Type
 enum Type {
     case bug
@@ -98,41 +101,50 @@ func ==(lhs: Move, rhs: Move) -> Bool {
 
 // http://bulbapedia.bulbagarden.net/wiki/Statistic
 struct Stats {
-    let hitpoints       : Int
-    let attack          : Int
-    let defense         : Int
-    let special_attack  : Int
-    let special_defense : Int
-    let speed           : Int
+    var hitpoints       : Int
+    var attack          : Int
+    var defense         : Int
+    var special_attack  : Int
+    var special_defense : Int
+    var speed           : Int
+}
+
+struct StatsModif {
+    let hitpoints       : Double
+    let attack          : Double
+    let defense         : Double
+    let special_attack  : Double
+    let special_defense : Double
+    let speed           : Double
 }
 
 // Collection to get stat modifiers based on nature
-let nature_modifiers: [Nature: Stats] = [
-   hardy: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
-   lonely: Stats(hitpoints:1, attack:1.1, defense:0.9, special_attack:1, special_defense:1, speed:1),
-   brave: Stats(hitpoints:1, attack:1.1, defense:1, special_attack:1, special_defense:1, speed:0.9),
-   adamant: Stats(hitpoints:1, attack:1.1, defense:1, special_attack:0.9, special_defense:1, speed:1),
-   naughty: Stats(hitpoints:1, attack:1.1, defense:1, special_attack:1, special_defense:0.9, speed:1),
-   bold: Stats(hitpoints:1, attack:0.9, defense:1.1, special_attack:1, special_defense:1, speed:1),
-   docile: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
-   relaxed: Stats(hitpoints:1, attack:1, defense:1.1, special_attack:1, special_defense:1, speed:0.9),
-   impish: Stats(hitpoints:1, attack:1, defense:1.1, special_attack:0.9, special_defense:1, speed:1),
-   lax: Stats(hitpoints:1, attack:1, defense:1.1, special_attack:1, special_defense:0.9, speed:1),
-   timid: Stats(hitpoints:1, attack:0.9, defense:1, special_attack:1, special_defense:1, speed:1.1),
-   hasty: Stats(hitpoints:1, attack:1, defense:0.9, special_attack:1, special_defense:1, speed:1.1),
-   serious: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
-   jolly: Stats(hitpoints:1, attack:1, defense:1, special_attack:0.9, special_defense:1, speed:1.1),
-   naive: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:0.9, speed:1.1),
-   modest: Stats(hitpoints:1, attack:0.9, defense:1, special_attack:1.1, special_defense:1, speed:1),
-   mild: Stats(hitpoints:1, attack:1, defense:0.9, special_attack:1.1, special_defense:1, speed:1),
-   quiet: Stats(hitpoints:1, attack:1, defense:1, special_attack:1.1, special_defense:1, speed:0.9),
-   bashful: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
-   rash: Stats(hitpoints:1, attack:1, defense:1, special_attack:1.1, special_defense:0.9, speed:1),
-   calm: Stats(hitpoints:1, attack:0.9, defense:1, special_attack:1, special_defense:1.1, speed:1),
-   gentle: Stats(hitpoints:1, attack:1, defense:0.9, special_attack:1, special_defense:1.1, speed:1),
-   sassy: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1.1, speed:0.9),
-   careful: Stats(hitpoints:1, attack:1, defense:1, special_attack:0.9, special_defense:1.1, speed:1),
-   quirky: Stats(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1)
+let nature_modifiers: [Nature: StatsModif] = [
+   .hardy: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
+   .lonely: StatsModif(hitpoints:1, attack:1.1, defense:0.9, special_attack:1, special_defense:1, speed:1),
+   .brave: StatsModif(hitpoints:1, attack:1.1, defense:1, special_attack:1, special_defense:1, speed:0.9),
+   .adamant: StatsModif(hitpoints:1, attack:1.1, defense:1, special_attack:0.9, special_defense:1, speed:1),
+   .naughty: StatsModif(hitpoints:1, attack:1.1, defense:1, special_attack:1, special_defense:0.9, speed:1),
+   .bold: StatsModif(hitpoints:1, attack:0.9, defense:1.1, special_attack:1, special_defense:1, speed:1),
+   .docile: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
+   .relaxed: StatsModif(hitpoints:1, attack:1, defense:1.1, special_attack:1, special_defense:1, speed:0.9),
+   .impish: StatsModif(hitpoints:1, attack:1, defense:1.1, special_attack:0.9, special_defense:1, speed:1),
+   .lax: StatsModif(hitpoints:1, attack:1, defense:1.1, special_attack:1, special_defense:0.9, speed:1),
+   .timid: StatsModif(hitpoints:1, attack:0.9, defense:1, special_attack:1, special_defense:1, speed:1.1),
+   .hasty: StatsModif(hitpoints:1, attack:1, defense:0.9, special_attack:1, special_defense:1, speed:1.1),
+   .serious: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
+   .jolly: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:0.9, special_defense:1, speed:1.1),
+   .naive: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:0.9, speed:1.1),
+   .modest: StatsModif(hitpoints:1, attack:0.9, defense:1, special_attack:1.1, special_defense:1, speed:1),
+   .mild: StatsModif(hitpoints:1, attack:1, defense:0.9, special_attack:1.1, special_defense:1, speed:1),
+   .quiet: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1.1, special_defense:1, speed:0.9),
+   .bashful: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1),
+   .rash: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1.1, special_defense:0.9, speed:1),
+   .calm: StatsModif(hitpoints:1, attack:0.9, defense:1, special_attack:1, special_defense:1.1, speed:1),
+   .gentle: StatsModif(hitpoints:1, attack:1, defense:0.9, special_attack:1, special_defense:1.1, speed:1),
+   .sassy: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1.1, speed:0.9),
+   .careful: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:0.9, special_defense:1.1, speed:1),
+   .quirky: StatsModif(hitpoints:1, attack:1, defense:1, special_attack:1, special_defense:1, speed:1)
 ]
 
 struct Species : Hashable {
@@ -223,9 +235,16 @@ let species_bulbasaur = Species(
    )
 )
 
-// Algorithm for effective stat computation (hitpoints excluded)
-func effective_stat_set(lvl L: Int, base B: Int, individual I: Int, effort E: Int, nat_mod N: Int){
-   Int(Int( Double((2 * B + I + E) * L) / Double(100) + 5) * N)
+// Algorithm for effective stat computation
+func effective_stat_set(lvl L: Int, base B: Int, individual I: Int, effort E: Int, nat_mod N: Double) -> Int {
+   let temp1 = Double((2 * B + I + E) * L)
+   let temp2 = floor(temp1 / Double(100) + 5) * N
+   return Int(temp2)
+}
+func effective_HP_set(lvl L: Int, base B: Int, individual I: Int, effort E: Int) -> Int {
+   let temp1 = Double((2 * B + I + E) * L)
+   let temp2 = Int(temp1 / Double(100))
+   return temp2 + L + 10
 }
 
 struct Pokemon {
@@ -243,22 +262,28 @@ struct Pokemon {
     var temp_modifiers    : Stats
     var effective_stats   : Stats {
       get {
+         let HP = effective_HP_set(lvl: level, base: species.base_values.hitpoints, individual: individual_values.hitpoints, effort: effort_values.hitpoints) + temp_modifiers.hitpoints
+         let att = effective_stat_set(lvl: level, base: species.base_values.attack, individual: individual_values.attack, effort: effort_values.attack, nat_mod: (nature_modifiers[nature]?.attack)!) + temp_modifiers.attack
+         let def = effective_stat_set(lvl: level, base: species.base_values.defense, individual: individual_values.defense, effort: effort_values.defense, nat_mod: (nature_modifiers[nature]?.defense)!) + temp_modifiers.defense
+         let spe_att = effective_stat_set(lvl: level, base: species.base_values.special_attack, individual: individual_values.special_attack, effort: effort_values.special_attack, nat_mod: (nature_modifiers[nature]?.special_attack)!) + temp_modifiers.special_attack
+         let spe_def = effective_stat_set(lvl: level, base: species.base_values.special_defense, individual: individual_values.special_defense, effort: effort_values.special_defense, nat_mod: (nature_modifiers[nature]?.special_defense)!) + temp_modifiers.special_defense
+         let spe = effective_stat_set(lvl: level, base: species.base_values.speed, individual: individual_values.speed, effort: effort_values.speed, nat_mod: (nature_modifiers[nature]?.speed)!) + temp_modifiers.speed
          return Stats(
-            hitpoints: Int(Double((2 * species.base_values.hitpoints + individual_values.hitpoints + effort_values.hitpoints) * level) / Double(100) + level + 10) + temp_modifiers.hitpoints,
-            attack: effective_stat_set(lvl: level, base: species.base_values.attack, individual: individual_values.attack, effort: effort_values.attack, nat_mod: nature_modifiers[nature].attack) + temp_modifiers.attack,
-            defense: effective_stat_set(lvl: level, base: species.base_values.defense, individual: individual_values.defense, effort: effort_values.defense, nat_mod: nature_modifiers[nature].defense) + temp_modifiers.defense,
-            special_attack: effective_stat_set(lvl: level, base: species.base_values.special_attack, individual: individual_values.special_attack, effort: effort_values.special_attack, nat_mod: nature_modifiers[nature].special_attack) + temp_modifiers.special_attack,
-            special_defense: effective_stat_set(lvl: level, base: species.base_values.special_defense, individual: individual_values.special_defense, effort: effort_values.special_defense, nat_mod: nature_modifiers[nature].special_defense) + temp_modifiers.special_defense,
-            speed: effective_stat_set(lvl: level, base: species.base_values.speed, individual: individual_values.speed, effort: effort_values.speed, nat_mod: nature_modifiers[nature].speed) + temp_modifiers.speed
+            hitpoints: HP,
+            attack: att,
+            defense: def,
+            special_attack: spe_att,
+            special_defense: spe_def,
+            speed: spe
          )
       }
       set(new_effective_stats){
-         temp_modifiers.hitpoints += new_effective_stats.hitpoints - effective_stats.hitpoints
-         temp_modifiers.attack += new_effective_stats.attack - effective_stats.attack
-         temp_modifiers.defense += new_effective_stats.defense - effective_stats.defense
-         temp_modifiers.special_attack += new_effective_stats.special_attack - effective_stats.special_attack
-         temp_modifiers.special_defense += new_effective_stats.special_defense - effective_stats.special_defense
-         temp_modifiers.speed += new_effective_stats.speed - effective_stats.speed
+         temp_modifiers.hitpoints = temp_modifiers.hitpoints + new_effective_stats.hitpoints - effective_stats.hitpoints
+         temp_modifiers.attack = temp_modifiers.attack + new_effective_stats.attack - effective_stats.attack
+         temp_modifiers.defense = temp_modifiers.defense + new_effective_stats.defense - effective_stats.defense
+         temp_modifiers.special_attack = temp_modifiers.special_attack + new_effective_stats.special_attack - effective_stats.special_attack
+         temp_modifiers.special_defense = temp_modifiers.special_defense + new_effective_stats.special_defense - effective_stats.special_defense
+         temp_modifiers.speed = temp_modifiers.speed + new_effective_stats.speed - effective_stats.speed
       }
     }
 }
@@ -273,9 +298,29 @@ struct Environment {
 }
 
 // http://bulbapedia.bulbagarden.net/wiki/Type/Type_chart
+let type_modifiers: [Type: [Type: Double]] = [
+   .normal: [.normal:1, .fighting:1, .flying:1, .poison:1, .ground:1, .rock:0.5, .bug:1, .ghost:0, .steel:0.5, .fire:1, .water:1, .grass:1, .electric:1, .psychic:1, .ice:1, .dragon:1, .dark:1, .fairy:1],
+   .fighting: [.normal:2, .fighting:1, .flying:0.5, .poison:0.5, .ground:1, .rock:2, .bug:0.5, .ghost:0, .steel:2, .fire:1, .water:1, .grass:1, .electric:1, .psychic:0.5, .ice:2, .dragon:1, .dark:2, .fairy:0.5],
+   .flying: [.normal:1, .fighting:2, .flying:1, .poison:1, .ground:1, .rock:0.5, .bug:2, .ghost:1, .steel:0.5, .fire:1, .water:1, .grass:2, .electric:0.5, .psychic:1, .ice:1, .dragon:1, .dark:1, .fairy:1],
+   .poison: [.normal:1, .fighting:1, .flying:1, .poison:0.5, .ground:0.5, .rock:0.5, .bug:1, .ghost:0.5, .steel:0, .fire:1, .water:1, .grass:2, .electric:1, .psychic:1, .ice:1, .dragon:1, .dark:1, .fairy:2],
+   .ground: [.normal:1, .fighting:1, .flying:0, .poison:2, .ground:1, .rock:2, .bug:0.5, .ghost:1, .steel:2, .fire:2, .water:1, .grass:0.5, .electric:2, .psychic:1, .ice:1, .dragon:1, .dark:1, .fairy:1],
+   .rock: [.normal:1, .fighting:0.5, .flying:2, .poison:1, .ground:0.5, .rock:1, .bug:2, .ghost:1, .steel:0.5, .fire:2, .water:1, .grass:1, .electric:1, .psychic:1, .ice:2, .dragon:1, .dark:1, .fairy:1],
+   .bug: [.normal:1, .fighting:0.5, .flying:0.5, .poison:0.5, .ground:1, .rock:1, .bug:1, .ghost:0.5, .steel:0.5, .fire:0.5, .water:1, .grass:2, .electric:1, .psychic:2, .ice:1, .dragon:1, .dark:2, .fairy:0.5],
+   .ghost: [.normal:0, .fighting:1, .flying:1, .poison:1, .ground:1, .rock:1, .bug:1, .ghost:2, .steel:1, .fire:1, .water:1, .grass:1, .electric:1, .psychic:2, .ice:1, .dragon:1, .dark:0.5, .fairy:1],
+   .steel: [.normal:1, .fighting:1, .flying:1, .poison:1, .ground:1, .rock:2, .bug:1, .ghost:1, .steel:0.5, .fire:0.5, .water:0.5, .grass:1, .electric:0.5, .psychic:1, .ice:2, .dragon:1, .dark:1, .fairy:2],
+   .fire: [.normal:1, .fighting:1, .flying:1, .poison:1, .ground:1, .rock:0.5, .bug:2, .ghost:1, .steel:2, .fire:0.5, .water:0.5, .grass:2, .electric:1, .psychic:1, .ice:2, .dragon:0.5, .dark:1, .fairy:1],
+   .water: [.normal:1, .fighting:1, .flying:1, .poison:1, .ground:2, .rock:2, .bug:1, .ghost:1, .steel:1, .fire:2, .water:0.5, .grass:0.5, .electric:1, .psychic:1, .ice:1, .dragon:0.5, .dark:1, .fairy:1],
+   .grass: [.normal:1, .fighting:1, .flying:0.5, .poison:0.5, .ground:2, .rock:2, .bug:0.5, .ghost:1, .steel:0.5, .fire:0.5, .water:2, .grass:0.5, .electric:1, .psychic:1, .ice:1, .dragon:0.5, .dark:1, .fairy:1],
+   .electric: [.normal:1, .fighting:1, .flying:2, .poison:1, .ground:0, .rock:1, .bug:1, .ghost:1, .steel:1, .fire:1, .water:2, .grass:0.5, .electric:0.5, .psychic:1, .ice:1, .dragon:0.5, .dark:1, .fairy:1],
+   .psychic: [.normal:1, .fighting:2, .flying:1, .poison:2, .ground:1, .rock:1, .bug:1, .ghost:1, .steel:0.5, .fire:1, .water:1, .grass:1, .electric:1, .psychic:0.5, .ice:1, .dragon:1, .dark:0, .fairy:1],
+   .ice: [.normal:1, .fighting:1, .flying:2, .poison:1, .ground:2, .rock:1, .bug:1, .ghost:1, .steel:0.5, .fire:0.5, .water:0.5, .grass:2, .electric:1, .psychic:1, .ice:0.5, .dragon:2, .dark:1, .fairy:1],
+   .dragon: [.normal:1, .fighting:1, .flying:1, .poison:1, .ground:1, .rock:1, .bug:1, .ghost:1, .steel:0.5, .fire:1, .water:1, .grass:1, .electric:1, .psychic:1, .ice:1, .dragon:2, .dark:1, .fairy:0],
+   .dark: [.normal:1, .fighting:0.5, .flying:1, .poison:1, .ground:1, .rock:1, .bug:1, .ghost:2, .steel:1, .fire:1, .water:1, .grass:1, .electric:1, .psychic:2, .ice:1, .dragon:1, .dark:0.5, .fairy:0.5],
+   .fairy: [.normal:1, .fighting:2, .flying:1, .poison:0.5, .ground:1, .rock:1, .bug:1, .ghost:1, .steel:0.5, .fire:0.5, .water:1, .grass:1, .electric:1, .psychic:1, .ice:1, .dragon:2, .dark:2, .fairy:1]
+]
+
 func typeModifier(attacking: Type, defending : Type) -> Double {
-    // TODO: encode type/type chart
-    return 1
+    return (type_modifiers[attacking]?[defending])!
 }
 
 // http://bulbapedia.bulbagarden.net/wiki/Damage
