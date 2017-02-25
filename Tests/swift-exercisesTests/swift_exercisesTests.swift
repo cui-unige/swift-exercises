40a6ef79fc13ac4d6740e6346d0668037f55272d
tullio iacobucci <awfkchan@gmail.com>
2017-02-25 17:25:41 +0100

    changed test to match change in source

--- Tests/swift-exercisesTests/swift_exercisesTests.swift
+++ Tests/swift-exercisesTests/swift_exercisesTests.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import swift_exercises
 
 class swift_exercisesTests: XCTestCase {
-  
+
     func test_typeModifier() {
-        XCTAssertEqual(typeModifier(attacking: Type.normal, defending: Type.normal), 1, "normal/normal should be 1")
+        XCTAssertEqual(typeModifier(attacking: Type.normal, defending: (Type.normal, nil)), 1, "normal/normal should be 1")
     }
 
     static var allTests : [(String, (swift_exercisesTests) -> () throws -> Void)] {
