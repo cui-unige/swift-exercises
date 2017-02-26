a20fa4cb191bd14ae66a15ab47f64b7764909076
CADER Aslam <doctor@localhost.localdomain>
2017-02-26 12:55:05 +0100

    effective stats fini

--- Sources/swift_exercises.swift
+++ Sources/swift_exercises.swift
@@ -125,10 +125,27 @@ func ==(lhs: Species, rhs: Species) -> Bool {
 // Do you use an enum, a map or constants/variables?
 // http://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number
 
+// Dracaufeu
+let moveAirSlash = Move(id: 1, name: "Air Slash", description: "Air Slash deals damage and has a 30% chance of causing the target to flinch", category: Category.special, type: Type.flying, power: 75, accuracy: 95, powerpoints: 20, priority: 0)
 
+let moveDragonClaw = Move(id: 2, name: "Dragon Claw", description: "Dragon Claw deals damage with no additional effect.", category: Category.physical, type: Type.dragon, power: 80, accuracy: 100, powerpoints: 15, priority: 0)
+
+let moveEmber = Move(id: 3, name: "Ember", description: "Ember deals damage and has a 10% chance of burning the target.", category: Category.special, type: Type.fire, power: 40, accuracy: 100, powerpoints: 25, priority: 0)
+
+let moveFlareBlitz = Move(id: 4, name: "Flare Blitz", description: "Flare Blitz deals damage and has a 10% chance of burning the target, but the user receives 1⁄3 of the damage it inflicted in recoil. In other words, if the attack does 90 HP damage to the opponent, the user will lose 30 HP.", category: Category.physical, type: Type.fire, power: 120, accuracy: 100, powerpoints: 15, priority: 0)
+
+let moveGrowl = Move(id: 5, name: "Growl", description: "Growl lowers the target's Attack by one stage.", category: Category.status, type: Type.normal, power: 0, accuracy: 100, powerpoints: 40, priority: 0)
+
+let moveHeatWave = Move(id: 6, name: "Heat Wave", description: "Heat Wave deals damage and has a 10% chance of burning the target.", category: Category.special, type: Type.fire, power: 95, accuracy: 90, priority: 0)
+
+let charizardStat = Stats(hitpoints: 0, attack: 0, defense: 0, special_attack: 3, special_defense: 0, speed: 0)
+
+let charizard = Species(id: 006, name: "Charizard", evolutions: [], attacks: [moveAirSlash, moveDragonClaw, moveEmber, moveFlareBlitz, moveGrowl, moveHeatWave], type: (Type.fire, nil), base_values: charizardStat)
+
+// Pikachu
 let pikachuStats = Stats(hitpoints: 35, attack: 55, defense: 30, special_attack: 50,special_defense: 40, speed: 90)
 
-let pikachuThunderShockAttack = Move(id: 84,
+let thunderShockAttack = Move(id: 84,
                                      name: "Thunder Shock",
                                      description: "paralyze target",
                                      category: special,
@@ -136,16 +153,25 @@ let pikachuThunderShockAttack = Move(id: 84,
                                      power:40,
                                      accuracy:100,
                                      powerpoints:30,
-                                     priority:100, ///////////// ???????????????,
+                                     priority:0 ///////////// ???????????????,
                                    )
 
 
 let pikachu = Species(id: 025,
                       name: "Pikachu",
-                      evolutions: [], // mettre raichu
-                      attacks: [pikachuThunderShockAttack],
+                      evolutions: [raichu],
+                      attacks: [thunderShockAttack],
                       type: (electric, nil),
-                      base_values: pikachuStats,
+                      base_values: pikachuStats
+              )
+let raichuStats = Stats(hitpoints: 60, attack: 90, defense: 55, special_attack: 90,special_defense: 80, speed: 110)
+
+let raichu = Species(id: 026,
+                      name: "Raichu",
+                      evolutions: [],
+                      attacks: [thunderShockAttack],
+                      type: (electric, nil),
+                      base_values: raichuStats
               )
 
 
@@ -161,6 +187,16 @@ struct Pokemon {
     let moves             : [Move: Int] // Move -> remaining powerpoints
     let individual_values : Stats
     let effort_values     : Stats
+    var effective_stats   : Stats { // effective_stats for Generations I and II)
+                              get {
+                                  let hitpoints: Int = ((((species.base_values.hitpoints + individual_values.hitpoints)*2 + ((effort_values.hitpoints).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + level + 10
+                                  let attack: Int = ((((species.base_values.attack + individual_values.attack)*2 + ((effort_values.attack).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + 5
+                                  let defense: Int = ((((species.base_values.defense + individual_values.defense)*2 + ((effort_values.defense).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + 5
+                                  let special_attack: Int = ((((species.base_values.special_attack + individual_values.special_attack)*2 + ((effort_values.special_attack).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + 5
+                                  let special_defense: Int = ((((species.base_values.special_defense + individual_values.special_defense)*2 + ((effort_values.special_defense).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + 5
+                                  let speed: Int = ((((species.base_values.speed + individual_values.speed)*2 + ((effort_values.speed).rounded(.up)/4).rounded(.down))* level)/100).round(.down) + 5
+                              }
+                            }
     // TODO: implement the effective stats as a computed property:
     // https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/Properties.html#//apple_ref/doc/uid/TP40014097-CH14-ID259
     // var effective_stats   : Stats {
@@ -185,10 +221,17 @@ func typeModifier(attacking: Type, defending : Type) -> Double {
 // http://bulbapedia.bulbagarden.net/wiki/Damage
 func damage(environment : Environment, pokemon: Pokemon, move: Move, target: Pokemon) -> Int {
     // TODO
-    return 0
+    //let damage_value = ( (2*pokemon.level+10)*
+    let damage_value = 0;
+    return damage_value;
 }
 
 struct State {
+      /*
+      Pokemon1: Pokemon;
+      Pokemon2: Pokemon;
+
+      */
     // TODO: describe a battle state
 }
 
