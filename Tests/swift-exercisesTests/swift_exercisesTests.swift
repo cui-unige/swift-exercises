af4da0171702a31b1f4ead3c66414fc3ef142f3e
Théo Giovanna <theo.giovanna@gmail.com>
2017-03-07 16:24:59 +0100

    final test

--- Tests/swift-exercisesTests/swift_exercisesTests.swift
+++ Tests/swift-exercisesTests/swift_exercisesTests.swift
@@ -5,6 +5,14 @@ class swift_exercisesTests: XCTestCase {
   
     func test_typeModifier() {
         XCTAssertEqual(typeModifier(attacking: Type.normal, defending: Type.normal), 1, "normal/normal should be 1")
+        print("\n ######################## \n")
+        print("\nPokemon v1. Giovanna Théo.")
+        print("You play the role of Bob. You can select your Pokemon and your moves")
+        print("Some moves can change the weather.")
+        print("Your opponent is Alice. Her pokemons and their moves are selected randomly.")
+        print("Moves have powerpoints decreasing of 1 each time they're being used.")
+        print("When a move does not have PP anymore, it is removed from the available choices.")
+        print("You can leave a battle or change your Pokemon within a battle.\n")
         go();
         
     }
