4f14606d9d70767470a4a9314930a23e933a05cf
Ornela Tchawou <Ornela.Tchawou@etu.unige.ch>
2017-03-19 17:27:17 +0100

    exo 3 rajoute

--- Sources/swift_exercises.swift
+++ Sources/swift_exercises.swift
@@ -18,6 +18,7 @@ enum Type {
     case rock
     case steel
     case water
+
 }
 
 // http://bulbapedia.bulbagarden.net/wiki/Damage_category
@@ -125,7 +126,7 @@ func ==(lhs: Species, rhs: Species) -> Bool {
 // Do you use an enum, a map or constants/variables?
 // http://bulbapedia.bulbagarden.net/wiki/List_of_Pokémon_by_National_Pokédex_number
 let pound_move = Move(id:1,name:"pound",description:"touch ",category:Category.physical,type:Type.normal,power:40,accuracy:100,powerpoints:35,priority:0)
-let karate_move = Move(id:2,name:"karate chop",description:"touch ",category:Category.physical,type:Type.fighting,power:50,accuracy:100,powerpoints:25,prioriy:0)
+let karate_move = Move(id:2,name:"karate chop",description:"touch ",category:Category.physical,type:Type.fighting,power:50,accuracy:100,powerpoints:25,priority:0)
 let ice_punch_move = Move(id:8,name:"ice punch",description:" beautiful",category:Category.physical,type:Type.normal,power:75,accuracy:100,powerpoints:15,priority:0)
 let fly_move = Move(id:19,name:"fly",description:" clever",category:Category.physical,type:Type.flying,power:90,accuracy:95,powerpoints:15,priority:0)
 let switch_move = Move(id:14,name:"Swords dance",description:" beautiful",category:Category.status,type:Type.normal,power:20,accuracy:0,powerpoints:0,priority:0)
@@ -133,7 +134,7 @@ let switch_move = Move(id:14,name:"Swords dance",description:" beautiful",catego
 let florizarre_specie = Species(id:001 ,name: "florizarre", evolutions:[],attacks:[pound_move,switch_move],type:( Type.poison ,nil), base_values:Stats(hitpoints:80,attack:82,defense:83,special_attack:100,special_defense:100,speed:80))
 let herbizarre_specie = Species(id:002, name:"herbizzare",evolutions:[florizarre_specie],attacks:[karate_move,fly_move],type:(Type.poison,nil),base_values:Stats(hitpoints:60,attack:62,defense:63,special_attack:80,special_defense:80,speed:60))
 let bulbizarre_specie = Species(id:003, name:"bulbizarre", evolutions:[florizarre_specie,herbizarre_specie],attacks:[pound_move,fly_move],type:(Type.poison,nil),base_values:Stats(hitpoints:45,attack:49,defense:49,special_attack:65,special_defense:65,speed:45) )
-let poissiroy_specie = Species(id:119,name:"poissiroy",evolutions:[],attacks:[switch_move ,pound_move],type:(Type.water,nil),base_values:Stats(hitpoints:140,atack:87,defense:63,special_attack:63,special_defense:76,speed:65))
+let poissiroy_specie = Species(id:119,name:"poissiroy",evolutions:[],attacks:[switch_move ,pound_move],type:(Type.water,nil),base_values:Stats(hitpoints:140,attack:87,defense:63,special_attack:63,special_defense:76,speed:65))
 
 
 struct Pokemon {
@@ -153,26 +154,22 @@ struct Pokemon {
     // var effective_stats   : Stats {
     // }
 
-    let base :Stats(Species.base_values.hitpoints,Species.base_values.attack,Species.base_values.defense,
-    Species.base_values.special_attack,Species.base_values.special_defense,Species.base_values.speed)
 
-    let iv :Stats(individual_values.hitpoints,individual_values.attack,individual_values.defense,
-    individual_values.special_attack,individual_values.special_defense,individual_values.speed)
+    func effective_values(b:Int , i:Int,  e:Int , levl : Int)-> Int{
 
-    let ev :Stats(effort_values.hitpoints,effort_values.attack,effort_values.defense,effort_values.special_attack,
-    effort_values.special_defense,effort_values.speed)
-
-    let effective_stats : Stats
-    func effective_values(b:int , i:int,  e:int , levl : int)-> int{
-
-      return int((((b + i)*2 + e/4)*levl/100)+5)
+      return Int((((b + i)*2 + e/4)*levl/100) + 5)
     }
-    let effective_stat.hitpoint = int((((base[0] + iv[0] )*2 + ev[0]/4)*level/100)+ level +10)
-    let effective_stat.attact = effective_values( base[1], iv[1], ev[1] , level)
-    let effective_stat.defense= effective_values(base[2],  iv[2], ev[2] ,level)
-    let effective_stat.special_attack = effective_values( base[3],  iv[3],  ev[3] , level)
-    let effective_stat.special_defense= effective_values( base[4],  iv[4],  ev[4] , level)
-    let effective_stat.speed = effective_values( base[5],  iv[5],  ev[5] , level)
+    let effective_stats = Stats.self
+
+
+    effective_stats.hitpoints = (((( species.base_values.hitpoints + individual_values.hitpoints )*2 +  effort_values.hitpoints/4)*level/100) + level + 10)
+    effective_stats.attack = effective_values( species.base_values.attack, individual_values.attack, effort_values.attack , level)
+    effective_stats.defense =  effective_values(species.base_values.defense,  individual_values.defense, effort_values.defense ,level)
+    effective_stats.special_attack =  effective_values( species.base_values.special_attack, individual_values.special_attack,  individual_values.special_attack , level)
+    effective_stats.special_defense =  effective_values( species.base_values.special_defense, individual_values.special_defense,  effort_values.special_defense , level)
+    effective_stats.speed =  effective_values( species.base_values.speed,  individual_values.speed,  effort_values.speed , level)
+
+
 }
 
 struct Trainer {
@@ -187,8 +184,22 @@ struct Environment {
 // http://bulbapedia.bulbagarden.net/wiki/Type/Type_chart
 func typeModifier(attacking: Type, defending : Type) -> Double {
     // TODO: encode type/type chart
-    return 1
+    typealias Modif = ( typ :Type , typ:Type,coeff :Double)
+    var  coeff_modif = [Modif]()
+    coeff_modif = [(.normal, .normal, 1), (.normal, .fighting, 1), (.normal, .flying, 1), (.normal, .poison, 1),
+  (.normal,.ground, 1), (.normal, .rock, 0.5), (.normal, .bug, 1), (.normal, .ghost, 0), (.normal, .steel, 0.5),
+  (.normal, .fire, 1), (.normal, .water, 1), (.normal, .grass, 1), (.normal, .electric, 1), (.normal, .psychic, 1),
+  (.normal, .ice, 1), (.normal, .dragon, 1), (.normal, .dark, 1), (.normal, .fairy, 1), (.fighting, .normal, 2),
+  (.fighting, .fighting, 1), (.fighting, .flying, 0.5), (.fighting, .poison, 0.5), (.fighting, .ground, 0.5), (.fighting, .rock, 2)]
+for i in 0 ... 23  {
+  if (attacking == coeff_modif[i].typ) && (defending == coeff_modif[i].typ) {
+  return Double ( coeff_modif[i].coeff)
+  }
 }
+
+
+  }
+
 
 // http://bulbapedia.bulbagarden.net/wiki/Damage
 func damage(environment : Environment, pokemon: Pokemon, move: Move, target: Pokemon) -> Int {
